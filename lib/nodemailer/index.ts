7ff26daa15d0fcb287640cfe3cf4e737e70763b0
priy1105 <priyanshukr5912@gmail.com>
2025-09-27 06:16:47 +0530

    Re-commit with added features

---
+++ lib/nodemailer/index.ts
@@ -0,0 +1,44 @@
+import nodemailer from 'nodemailer';
+import {WELCOME_EMAIL_TEMPLATE, NEWS_SUMMARY_EMAIL_TEMPLATE} from "@/lib/nodemailer/templates";
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.NODEMAILER_EMAIL!,
+        pass: process.env.NODEMAILER_PASSWORD!,
+    }
+})
+
+export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
+    const htmlTemplate = WELCOME_EMAIL_TEMPLATE
+        .replace('{{name}}', name)
+        .replace('{{intro}}', intro);
+
+    const mailOptions = {
+        from: `"Stocks_Analyzer.AI" <Stocks_Analyzer@priyanshu.kr>`,
+        to: email,
+        subject: `Welcome to Stocks_Analyzer.AI - Your stock market toolkit is ready!`,
+        text: 'Thanks for joining Stocks_Analyzer.AI',
+        html: htmlTemplate,
+    }
+
+    await transporter.sendMail(mailOptions);
+}
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Stocks_Analyzer.AI News" <stocks_analyzer@priyanshu.kr>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Stocks_Analyzer.AI`,
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
---
+++ postcss.config.mjs
@@ -0,0 +1,15 @@
+const config = {
+  plugins: {
+  "@tailwindcss/postcss": {},
+},
+};
+
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+export default config;
